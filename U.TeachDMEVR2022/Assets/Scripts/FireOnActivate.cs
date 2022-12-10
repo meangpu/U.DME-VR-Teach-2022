df838d6c4c7ee4245725accbfe183e15350b01ba
meangpu <51825872+meangpu@users.noreply.github.com>
2022-12-11 01:59:53 +0700

    finish gun bullet

---
+++ U.TeachDMEVR2022/Assets/Scripts/FireOnActivate.cs
@@ -0,0 +1,26 @@
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine;
+
+public class FireOnActivate : MonoBehaviour
+{
+    [SerializeField] GameObject _bulletPrefab;
+    [SerializeField] Transform _bulletSpawnPoint;
+    [SerializeField] float _bulletSpeed;
+    [SerializeField] float _destroyBulletAfterSecond;
+
+    void Start()
+    {
+        XRGrabInteractable grabAbleScpt = GetComponent<XRGrabInteractable>();
+        grabAbleScpt.activated.AddListener(FireBullet);
+    }
+
+
+    void FireBullet(ActivateEventArgs arg)
+    {
+        GameObject newBullet = Instantiate(_bulletPrefab);
+        newBullet.transform.position = _bulletSpawnPoint.position;
+        newBullet.GetComponent<Rigidbody>().velocity = _bulletSpawnPoint.forward * _bulletSpeed;
+        Destroy(newBullet, _destroyBulletAfterSecond);
+
+    }
+}
